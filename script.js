b888e6ca4712559474a237e41f996d455b210535
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-05-26 15:22:43 +0200

    fine

---
+++ script.js
@@ -0,0 +1,12 @@
+var nome;
+var cognome;
+var colore;
+var password;
+
+nome = prompt('Inserisci il tuo nome');
+
+cognome = prompt('Inserirci il tuo cognome');
+
+colore = prompt('Inserisci il tuo colore preferito');
+
+alert(nome+cognome+colore+'21');
